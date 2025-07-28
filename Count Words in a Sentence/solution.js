e35341b10952e108b9a8041763bdb9c80ea0d993
Devendra-23 <dev6dhokare@gmail.com>
2025-07-28 18:36:39 +0530

    Day 6

---
+++ Count Words in a Sentence/solution.js
@@ -0,0 +1,5 @@
+function wordCount(sentence) {
+  return sentence.split(" ").length;
+}
+
+console.log(wordCount("I love programming in Python")); // ➝ 5
---
+++ Count Words in a Sentence/solution.py
@@ -0,0 +1,4 @@
+def word_count(sentence):
+    return len(sentence.split())
+
+print(word_count("I love programming in Python"))  # ➝ 5
